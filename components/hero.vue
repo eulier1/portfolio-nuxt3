25fd34183f0d7677b66bb1fddc7d8ff988e4d920
Eulier <eulier@toolboxos.com>
2023-06-07 22:00:24 -0300

    add header dark theme

--- components/hero.vue
+++ components/hero.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="bg-white">
+  <div class="bg-stone-800 min-h-screen">
     <header class="absolute inset-x-0 top-0 z-50">
       <nav
         class="flex items-center justify-between p-6 lg:px-8"
         aria-label="Global"
       >
         <div class="flex lg:flex-1">
-          <a href="#" class="-m-1.5 p-1.5">
+          <a href="#" class="-m-1.5 p-1.5 text-stone-100">
             <span class="sr-only">Your Company</span>
             <img class="h-8 w-auto" src="" alt="" />
           </a>
@@ -26,7 +26,7 @@
             v-for="item in navigation"
             :key="item.name"
             :href="item.href"
-            class="text-sm font-semibold leading-6 text-gray-900"
+            class="text-sm font-semibold leading-6 text-stone-400"
             >{{ item.name }}</a
           >
         </div>
@@ -75,54 +75,59 @@
     </header>
 
     <div class="relative isolate px-6 pt-14 lg:px-8">
-      <div
-        class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-        aria-hidden="true"
-      >
-        <div
-          class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-          style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
-        />
-      </div>
-      <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-        <div class="hidden sm:mb-8 sm:flex sm:justify-center">
-          <div
-            class="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20"
-          >
-            Announcing our next round of funding.
-            <a href="#" class="font-semibold text-indigo-600"
-              ><span class="absolute inset-0" aria-hidden="true" />Read more
-              <span aria-hidden="true">&rarr;</span></a
-            >
+      <div class="xs:mx-auto py-32 sm:py-48 lg:py-56">
+        <div class="lg:hidden">
+          <div class="text-center">
+            <h2 class="text-2xl">
+              I'M
+              <h1
+                class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl pb-2"
+              >
+                Eulier Gonzalez
+              </h1>
+              Full-stack Js/Ts Developer & Consultant
+            </h2>
+            <div class="mt-10 flex items-center justify-center gap-x-6">
+              <button
+                class="rounded border-2 border-amber-400 text-amber-300 p-2.5"
+              >
+                Contact Me
+              </button>
+            </div>
           </div>
         </div>
-        <div class="text-center">
-          <h2 class="text-2xl">
-            I'M
-            <h1
-              class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl pb-2"
-            >
-              Eulier Gonzalez
-            </h1>
-            Full-stack Js/Ts Developer & Consultant
-          </h2>
-          <div class="mt-10 flex items-center justify-center gap-x-6">
-            <button
-              class="rounded border-2 border-amber-400 text-amber-300 p-2.5"
-            >
-              Contact Me
-            </button>
+        <div class="hidden lg:flex lg:justify-around">
+          <div class="text-center text-stone-400">
+            <h2 class="text-2xl">
+              I'M
+              <span
+                class="text-2xl tracking-tight text-amber-300 opacity-60 sm:text-6xl"
+              >
+                Eulier González
+              </span>
+            </h2>
+            <h2 class="text-2xl mt-4">
+              Full-stack Js/Ts Developer & Consultant
+            </h2>
+            <div class="mt-10 text-left">
+              <button
+                class="rounded border-2 border-amber-400 text-amber-300 p-2.5 "
+              >
+                Contact Me
+              </button>
+            </div>
+          </div>
+          <div class="text-center text-stone-400">
+            <div class="relative">
+              <img
+                class="image-header"
+                src="~/assets/img/20230318_134446.jpg"
+                alt=""
+              />
+            </div>
+            <div class=""></div>
           </div>
         </div>
-      </div>
-      <div
-        class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-        aria-hidden="true"
-      >
-        <div
-          class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-          style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
-        />
       </div>
     </div>
   </div>
@@ -141,3 +146,14 @@
   
   const mobileMenuOpen = ref(false)
   </script>
+
+<style>
+.image-header {
+  position: absolute;
+  transform: translate(-60%, -40%);
+  max-width: 100vw;
+  height: 100vh;
+  object-fit: contain;
+  z-index: -999;
+}
+</style>
