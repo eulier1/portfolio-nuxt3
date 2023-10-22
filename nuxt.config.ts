ab717ab6552083cd8aa3ddc5da982d745f20d155
Eulier <eulier@toolboxos.com>
2023-10-22 13:03:00 -0300

    fix property in nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,5 +4,7 @@ export default defineNuxtConfig({
   modules: [
     '@nuxtjs/tailwindcss'
   ],
-  buildAssetsDir: `/${process.env.BUILD_ASSETS_DIR}/_nuxt/`
+  app: {
+    buildAssetsDir: `/${process.env.BUILD_ASSETS_DIR}/_nuxt/`
+  }
 })
