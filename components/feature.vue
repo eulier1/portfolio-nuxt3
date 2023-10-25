aae5b8fac40473d25956ada18dec9edb336af795
Eulier <eulier@toolboxos.com>
2023-10-25 19:10:37 -0300

    add timeline and learning experience to project object

--- components/feature.vue
+++ components/feature.vue
@@ -4,8 +4,8 @@
     <div class="mx-auto max-w-2xl lg:text-center">
       <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl" >Recent Projects</p>
     </div>
-    <div class="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
-      <dl class="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+    <div class="md:flex mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl pl-4">
+      <!-- <dl class="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
         <div v-for="(project, index) in projects" :key="index+1000" class="relative pl-8">
           <dt class="text-base font-semibold leading-7 text-gray-900">
             {{ project.name }}
@@ -32,7 +32,85 @@
             <a :href="project.description[0].blog[0].url" target="_blank" class="underline"> {{ project.description[0].blog[0].name }} </a>  
           </dd>
         </div>
-      </dl>
+      </dl> -->
+      <div class="w-full md:w-1/2">
+        <p class="mt-2 mb-6 text-xl font-bold tracking-tight text-gray-900 sm:text-2xl" >Current</p>
+
+        <ol v-for="(project, index) in projects" :key="index+1000" class="relative border-l border-gray-200 dark:border-gray-700">  
+                          
+            <li v-if="!project.endDate" class="mb-10 ml-6">            
+                <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+                  📝
+                </span>
+                <a :href="project.description[0].url" target="_blank" class="underline">
+                  <h3 class="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">{{ project.name }}<span class="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ml-3"></span></h3>
+                </a>
+                <a v-if="project.companyUrl" target="_blank" class="underline" :href="project.companyUrl">
+                  <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">{{ project.companyName }}</p>
+                </a>
+                <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">{{ project.startDate }} - {{ project.endDate }} </time>
+                <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">{{ project.description[0].details }}</p>
+                <p v-if="project.description[0].technicalDetails" class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+                <span v-for="(technicalDetail, index) in project.description[0].technicalDetails">
+                  <ul>{{ technicalDetail }}</ul>
+                </span>
+                </p>
+                <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400"> {{ project.description[0].tech }}</p>
+
+              <div class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+                <a :href="project.description[0].url" target="_blank" class="underline">
+                  URL</a>
+              </div> 
+              <div class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400"> 
+                <a v-if="project.description[0].demoUrl" target="_blank" class="underline" :href="project.description[0].demoUrl"> 
+                  Demo URL 
+                </a>
+              </div> 
+              <div v-if="project.description[0].blog" class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+                <a :href="project.description[0].blog[0].url" target="_blank" class="underline"> {{ project.description[0].blog[0].name }}</a>
+              </div>
+            </li>
+        </ol>
+      </div>
+      <div class="w-full md:w-1/2">
+        <p class="mt-2 mb-6 text-xl font-bold tracking-tight text-gray-900 sm:text-2xl" >Done</p>
+
+        <ol v-for="(project, index) in projects" :key="index+1000" class="relative border-l border-gray-200 dark:border-gray-700">                  
+          <li v-if="project.endDate" class="mb-10 ml-6">            
+              <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+                ✅
+              </span>
+              <h3 class="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">{{ project.name }}<span class="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ml-3"></span></h3>
+              <a v-if="project.companyUrl" target="_blank" class="underline" :href="project.companyUrl">
+                <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">{{ project.companyName }}</p>
+              </a>
+              <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">{{ project.startDate }} - {{ project.endDate }} </time>
+              <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">{{ project.description[0].details }}</p>
+              <p v-if="project.description[0].technicalDetails" class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+              <span v-for="(technicalDetail, index) in project.description[0].technicalDetails">
+                <ul>{{ technicalDetail }}</ul>
+              </span>
+              </p>
+              <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400"> {{ project.description[0].tech }}</p>
+
+            <div class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+              <a :href="project.description[0].url" target="_blank" class="underline">
+                URL</a>
+            </div> 
+            <div class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400"> 
+              <a v-if="project.description[0].demoUrl" target="_blank" class="underline" :href="project.description[0].demoUrl"> 
+                Demo URL 
+              </a>
+            </div> 
+            <div v-if="project.description[0].blog" class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+              <a :href="project.description[0].blog[0].url" target="_blank" class="underline"> {{ project.description[0].blog[0].name }}</a>
+            </div>
+        </li>
+        </ol>
+      </div>
+
+
+
     </div>
   </div>
 </div>
--- server/api/projects.js
+++ server/api/projects.js
@@ -28,12 +28,8 @@
       thumbnail: 'https://i.imgur.com/d2VLGih.jpg',
       thumbnailXl: 'https://i.imgur.com/d2VLGih.jpg',
       role: 'Front-end',
-      techStack: [
-        { icon: 'html5', name: 'Html', url: 'https://developer.mozilla.org/en-US/docs/Web/HTML' },
-        { icon: 'css3-alt', name: 'CSS', url: 'https://developer.mozilla.org/en-US/docs/Web/CSS' },
-        { icon: 'js', name: 'Javascript', url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript' },
-        { icon: 'node-js', name: 'Nodejs', url: 'https://nodejs.org' },
-        { icon: 'angular', name: 'Angular', url: 'https://angular.io/' }],
+      isJobExperience: true,
+      isLearningExperience: false
     },
     {
       id: 1,
@@ -48,16 +44,8 @@
       }],
       thumbnail: 'https://i.imgur.com/oN67948.jpg',
       thumbnailXl: 'https://i.imgur.com/oN67948.jpg',
-      role: 'Front-end',
-      techStack: [
-        { icon: 'html5', name: 'Html', url: 'https://developer.mozilla.org/en-US/docs/Web/HTML' },
-        { icon: 'css3-alt', name: 'CSS', url: 'https://developer.mozilla.org/en-US/docs/Web/CSS' },
-        { icon: 'js', name: 'Javascript', url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript' },
-        { icon: 'node-js', name: 'Nodejs', url: 'https://nodejs.org' },
-        { icon: 'vuejs', name: 'Vuejs', url: 'https://vuejs.org/' },
-        { icon: 'c-inkscape', name: 'Inkscape', url: 'https://inkscape.org/' },
-        { icon: 'c-gsap', name: 'GSAP', url: 'https://greensock.com/gsap/' },
-      ],
+      isJobExperience: false,
+      isLearningExperience: true
     },
     // {
     //   id: 2,
@@ -83,21 +71,16 @@
       thumbnail: 'https://i.imgur.com/h43FjC0.jpg',
       thumbnailXl: 'https://i.imgur.com/BiuItHK.jpg',
       role: 'Front-end',
-      techStack: [
-        { icon: 'html5', name: 'Html', url: 'https://developer.mozilla.org/en-US/docs/Web/HTML' },
-        { icon: 'css3-alt', name: 'CSS', url: 'https://developer.mozilla.org/en-US/docs/Web/CSS' },
-        { icon: 'js', name: 'Javascript', url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript' },
-        { icon: 'node-js', name: 'Nodejs', url: 'https://nodejs.org' },
-        { icon: 'vuejs', name: 'Vuejs', url: 'https://vuejs.org/' },
-        { icon: 'c-nuxt', name: 'Nuxt', url: 'https://nuxtjs.org/' },
-      ],
+      isJobExperience: false,
+      isLearningExperience: true
     },
     {
       id: 4,
       name: 'Henry Miller Minister',
       companyName: 'SelvinCo',
       startDate: 'Mar 2018',
-      endDate: 'Mar 2018',
+      endDate: 'Mar 2019',
+      companyUrl: 'https://www.linkedin.com/company/selvin-co/?originalSubdomain=es',
       description: [ {
         details: "Church Donate's Website" ,
         tech: "Tech : Vue 2, Node.js, Laravel & MySQL",
@@ -106,23 +89,16 @@
       thumbnail: 'https://i.imgur.com/c8S3jo6.jpg',
       thumbnailXl: 'https://i.imgur.com/5cGxalE.jpg',
       role: 'Front-end/Back-end ',
-      techStack: [
-        { icon: 'html5', name: 'Html', url: 'https://developer.mozilla.org/en-US/docs/Web/HTML' },
-        { icon: 'css3-alt', name: 'CSS', url: 'https://developer.mozilla.org/en-US/docs/Web/CSS' },
-        { icon: 'js', name: 'Javascript', url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript' },
-        { icon: 'node-js', name: 'Nodejs', url: 'https://nodejs.org' },
-        { icon: 'vuejs', name: 'Vuejs', url: 'https://vuejs.org/' },
-        { icon: 'php', name: 'PHP', url: 'https://www.php.net/' },
-        { icon: 'laravel', name: 'Laravel', url: 'https://laravel.com/' },
-        { icon: 'c-mysql', name: 'MySQL', url: 'https://www.mysql.com/' },
-      ],
+      isJobExperience: true,
+      isLearningExperience: false
     },
     {
       id: 5,
       name: 'Cargill Image Map',
       companyName: 'SelvinCo',
       startDate: 'Mar 2018',
-      endDate: 'Mar 2018',
+      endDate: 'Mar 2019',
+      companyUrl: 'https://www.linkedin.com/company/selvin-co/?originalSubdomain=es',
       description: [ {
         details: "Cargill map for kiosk" ,
         tech: "Tech : Vue 2, Node.js, Laravel & MySQL",
@@ -141,13 +117,25 @@
         { icon: 'laravel', name: 'Laravel', url: 'https://laravel.com/' },
         { icon: 'c-mysql', name: 'MySQL', url: 'https://www.mysql.com/' },
       ],
+      isJobExperience: true,
+      isLearningExperience: false
     },
-    // {
-    //   id: 6,
-    //   name: 'My contact?',
-    //   description: 'click to send me an 📨',
-    //   url: 'mailto:eulier1@gmail.com',
-    // },
+    {
+      id: 6,
+      name: 'Deep Learning Specialization',
+      companyName: 'Coursera',
+      startDate: 'Mar 2019',
+      endDate: 'Mar 2020',
+      companyUrl: 'https://www.coursera.org/',
+      description: [ {
+        details: "1. Neural Networks Fundamentals - 2. Structuring Machine Learning Projects - 3. Deep Learning Network: Hyperparamenter tunning, Regularization & Optimizations - 4. Convolutional Neural Networks in TensorFlow - 5. Natural Languages Processing in TensorFlow" ,
+        tech: "Tech : Python, Pandas, TensorFlow",
+        url: 'https://www.coursera.org/account/accomplishments/specialization/certificate/ZYLU6G5DF253',
+      }],
+      url: '',
+      isJobExperience: false,
+      isLearningExperience: true
+    },
     // {
     //   id: 7,
     //   name: 'Moses Walker Jr',
@@ -189,11 +177,12 @@
     //   ],
     // },
     {
-      id: 9,
+      id: 7,
       name: 'Shipment truck tracking & waiting line system',
       companyName: 'Blackfly',
       startDate: 'Dec 2019',
       endDate: 'Apr 2020',
+      companyUrl: 'https://blackfly.dev',
       description: [ {
         details: "Dashboard to keep track trucks waiting outside a yard" ,
         tech: "Tech : Vue 2, Node.js, Node.js, Jest, Cypress",
@@ -202,17 +191,40 @@
       thumbnail: 'https://i.imgur.com/uKOzCLC.jpg',
       thumbnailXl: 'https://i.imgur.com/uKOzCLC.jpg',
       role: 'Front-end Architect & Tester',
-      techStack: [
-        { icon: 'html5', name: 'Html', url: 'https://developer.mozilla.org/en-US/docs/Web/HTML' },
-        { icon: 'css3-alt', name: 'CSS', url: 'https://developer.mozilla.org/en-US/docs/Web/CSS' },
-        { icon: 'js', name: 'Javascript', url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript' },
-        { icon: 'node-js', name: 'Nodejs', url: 'https://nodejs.org' },
-        { icon: 'vuejs', name: 'Vuejs', url: 'https://vuejs.org/' },
-        { icon: 'c-nuxt', name: 'Nuxt', url: 'https://nuxtjs.org/' },
-        { icon: 'typescript', name: 'Typescript', url: 'https://www.typescriptlang.org/' },
-        { icon: 'jest', name: 'Jest', url: 'https://jestjs.io/' },
-        { icon: 'cypress', name: 'Cypress', url: 'https://www.cypress.io/' },
-      ],
+      isJobExperience: true,
+      isLearningExperience: false
+    },
+    {
+      id: 8,
+      name: 'Js Advanced Concepts - Course',
+      companyName: 'Zero To Mastery',
+      startDate: 'Apr 2020',
+      endDate: 'Jan 2021',
+      companyUrl: 'https://zerotomastery.io/courses/advanced-javascript-concepts/',
+      description: [ {
+        details: "Data Structures & Algorithms, Javascript, V8 Architecture & Event Loop" ,
+        tech: "Tech : Javascript",
+        url: 'https://www.udemy.com/certificate/UC-de24a54c-e4f4-4f6d-8175-c83b3f59f583/',
+      }],
+      role: 'Software Engineer',
+      isJobExperience: false,
+      isLearningExperience: true
+    },
+    {
+      id: 9,
+      name: 'Senior Web Tools - Course',
+      companyName: 'Zero To Mastery',
+      startDate: 'Apr 2020',
+      endDate: 'Jan 2021',
+      companyUrl: 'https://zerotomastery.io/courses/junior-to-senior-web-developer-roadmap/',
+      description: [ {
+        details: "SSH, Git, Node, Npm, Http Server, Express, Cluster, Postgres & MongoDB (Persistant Storage A.K.A DBs), Security, Docker vs VM, CI/CD" ,
+        tech: "Tech : Javascript, Event Loop & V8 ",
+        url: 'https://www.udemy.com/certificate/UC-2e82f641-39e4-483d-b0b2-0e9271a721f4/',
+      }],
+      role: 'Software Engineer',
+      isJobExperience: false,
+      isLearningExperience: true
     },
     {
       id: 10,
@@ -220,6 +232,7 @@
       companyName: 'ToolboxOS',
       startDate: 'Apr 2021',
       endDate: 'May 2022',
+      companyUrl: 'https://www.toolboxos.com/',
       description: [ {
         details: "A chat-app to connect coaches with students" ,
         tech: `Tech : Vue 3 on the client (Composition API, Typescript, Jest, Storybook
@@ -231,29 +244,75 @@
       thumbnail: '',
       thumbnailXl: '',
       role: 'Software Engineer',
-      techStack: [
-        { icon: 'html5', name: 'Html', url: 'https://developer.mozilla.org/en-US/docs/Web/HTML' },
-        { icon: 'css3-alt', name: 'CSS', url: 'https://developer.mozilla.org/en-US/docs/Web/CSS' },
-        { icon: 'js', name: 'Javascript', url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript' },
-        { icon: 'node-js', name: 'Nodejs', url: 'https://nodejs.org' },
-        { icon: 'vuejs', name: 'Vuejs', url: 'https://vuejs.org/' },
-        { icon: 'c-nuxt', name: 'Nuxt', url: 'https://nuxtjs.org/' },
-        { icon: 'typescript', name: 'Typescript', url: 'https://www.typescriptlang.org/' },
-        { icon: 'jest', name: 'Jest', url: 'https://jestjs.io/' },
-        { icon: 'cypress', name: 'Cypress', url: 'https://www.cypress.io/' },
-      ],
+      isJobExperience: false,
+      isLearningExperience: true
     },
     {
       id: 11,
+      name: 'Learning Season: Node.js',
+      companyName: 'Zero To Mastery',
+      startDate: 'Ago 2021',
+      endDate: 'Nov 2022',
+      companyUrl: 'https://zerotomastery.io/courses/learn-node-js/',
+      description: [ {
+        details: "npm, mvc patterns, http server, express, module system (commonjs & esm), cluster & workers, authentication & authorization, security, docker, CI/CD with Github Actions, GraphQL, websockets with Socket.io " ,
+        tech: `Node, Express, Postgres, MongoDB, Passport, Helmet, Docker, GraphQL, Socket.io`,
+        url: 'https://www.youtube.com/playlist?list=PL_sapWkWdZHFc8HIN6GzhaoNDOLUruery',
+      }],
+      thumbnail: '',
+      thumbnailXl: '',
+      role: 'Business Consultant',
+      isJobExperience: false,
+      isLearningExperience: true
+    },
+    {
+      id: 12,
+      name: 'Learning Season: Business Consultant',
+      companyName: 'Zero To Mastery',
+      startDate: 'Feb 2022',
+      endDate: '',
+      companyUrl: 'https://zerotomastery.io/courses/learn-freelance-consulting/',
+      description: [ {
+        details: "Fundamentals as a Business Consultant, How to get started, How to find & land high paying consultant clients, How to conduct a research, Develop a personal brand" ,
+        tech: `The importance to solve problems that provide value to individuals & companies`,
+        url: 'https://www.youtube.com/playlist?list=PL_sapWkWdZHEF9Ob3gGYIBtqXgVQuJU29',
+      }],
+      thumbnail: '',
+      thumbnailXl: '',
+      role: 'Business Consultant',
+      isJobExperience: false,
+      isLearningExperience: true
+    },
+    {
+      id: 13,
+      name: 'Learning Season: Finances & Investing',
+      companyName: 'MorningStar',
+      startDate: 'Ene 2023',
+      endDate: '',
+      companyUrl: 'https://www.morningstar.com/start-investing/classroom',
+      description: [ {
+        details: "Following the Morning Star Investing Classroom" ,
+        tech: `The importance to solve problems that provide value to individuals & companies`,
+        url: 'https://www.youtube.com/playlist?list=PL_sapWkWdZHHVphkFirUx6ry78Os8fuh3',
+      }],
+      thumbnail: '',
+      thumbnailXl: '',
+      role: 'Investor',
+      isJobExperience: false,
+      isLearningExperience: true
+    },
+    {
+      id: 14,
       name: 'Shopify App Theme Extension',
       companyName: 'Bolle-Digital Inc',
+      companyUrl: 'https://getrecipekit.com/',
       startDate: 'Jun 2023',
       endDate: 'Oct 2023',
       description: [ {
         details: "Improving Web Vitals (LCP) in a Shopify App Theme Extension, for Merchants.",
         technicalDetails: ["Asset weight reduction ~29.17%", "Time invested ~27.5 Hrs"],
         tech: `Tech : Shopify App Theme Extension, Online Store, Admin Dashboard for Merchants, Vue 2, Nuxt 2, Node.js`,
-        url: 'https://jurni-landing-dev.web.app/',
+        url: 'https://getrecipekit.com/',
         demoUrl: 'https://youtu.be/-bHVIeHPUdw?si=2Tst1o7XB0ZRthZz',
         blog : [{
           name: 'Technical details description',
@@ -274,6 +333,8 @@
         { icon: 'jest', name: 'Jest', url: 'https://jestjs.io/' },
         { icon: 'cypress', name: 'Cypress', url: 'https://www.cypress.io/' },
       ],
+      isJobExperience: true,
+      isLearningExperience: false
     }
 
   ]
